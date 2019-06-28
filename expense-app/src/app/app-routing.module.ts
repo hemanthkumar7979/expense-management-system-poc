c2d05c5a75d7cfce6d69345517c4cd0e1fb7e19b
hemanthkumar7979 <hemanthkumar.sampangi@gmail.com>
2019-06-28 14:06:29 +0530

    Routing and Setting static page content

--- expense-app/src/app/app-routing.module.ts
+++ expense-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SettingComponent } from './setting/setting.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home' ,  component: HomeComponent},
+  {path: 'setting' , component: SettingComponent },
+  {path: 'profile' , component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- expense-app/src/app/app.module.ts
+++ expense-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SettingComponent } from './setting/setting.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
